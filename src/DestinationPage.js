77558fb41c917f4dc55b9696454eb7dd634cb324
bolu <bolu.olubajo@and.digital>
2023-12-13 16:39:59 +0100

    added the dummy destination page

---
+++ src/DestinationPage.js
@@ -0,0 +1,21 @@
+import React from "react";
+import "./DestinationPage.css";
+import DestinationPageVideo from "./resources/destination_page_background_video.mp4";
+
+export const DestinationPage = () => {
+  return (
+    <div className="header">
+      <header>
+        <video autoPlay loop muted>
+          <source src={DestinationPageVideo} type="video/mp4" />
+        </video>
+        <div className="content">
+          <h1>Provo, Turks and Caicos Islands</h1>
+          <p>This is the place for you! Based on your answers</p>
+          <button>Discover more</button>
+        </div>
+      </header>
+    </div>
+  );
+};
+export default DestinationPage;
